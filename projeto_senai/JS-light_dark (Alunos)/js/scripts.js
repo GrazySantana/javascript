51668c041eaf07a920772054810bd8a5eed259ef
GrazySantana <grazyyc1155@gmail.com>
2023-11-16 16:35:17 -0300

    .
    .

--- projeto_senai/JS-light_dark (Alunos)/js/scripts.js
+++ projeto_senai/JS-light_dark (Alunos)/js/scripts.js
@@ -1,1 +1,26 @@
-// Digitar JS aqui
+const changeThemeBtn = document.querySelector("#change-theme");
+
+// Função para alternar o modo escuro (dark mode)
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+    
+    if(darkMode) {
+        toggleDarkMode();
+    }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+    toggleDarkMode();
+
+    localStorage.removeItem("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    }
+});
